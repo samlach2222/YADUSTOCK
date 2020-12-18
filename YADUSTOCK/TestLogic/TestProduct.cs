1f1297146ced164d0beb85f06aab6b58ed5bc8ff
Zok0ta <65073455+Zok0ta@users.noreply.github.com>
2020-12-18 12:54:48 +0100

    Revert "Plein de bazzare"
    This reverts commit 170dc8afa96eb89a10cd72964394b8f43435f8b7.

--- YADUSTOCK/TestLogic/TestProduct.cs
+++ YADUSTOCK/TestLogic/TestProduct.cs
@@ -18,5 +18,20 @@ namespace TestLogic
             Assert.Equal(6, product.Quantity);
             Assert.Equal(0, product.ResalePrice);
         }
+
+        [Fact]
+        public void TestListProduct()
+        {
+            CreateProduct p = new Product(TypeProduct.POKEMON, 2, 6);
+            CreateProduct p2 = new Product(TypeProduct.CARTE, 1, 7);
+
+            List<Product> d = p.CreateListProduct();
+            List<Product> d2 = p2.CreateListProduct();
+
+            Assert.NotEmpty(d);
+            Assert.NotEmpty(d2);
+            Assert.Equal(d.ElementAt(0), p);
+            Assert.Equal(d2.ElementAt(0), p2);
+        }
     }
 }
