43ca3b2b877d0a152689278f5769dbeae5acd120
Samlach22 <samlach22@gmail.com>
2021-01-09 18:29:46 +0100

    Ajout du déplacement des popups
    Ajout du déplacements des fenètres suivantes
    - UI_ClosingWarning
    - UI_Parameters

--- YADUSTOCK/YADUSTOCK/UI_Parameters.xaml.cs
+++ YADUSTOCK/YADUSTOCK/UI_Parameters.xaml.cs
@@ -86,5 +86,11 @@ namespace YADUSTOCK
             window.ButtonClickSound();
             this.Close();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
     }
 }
