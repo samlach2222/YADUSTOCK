8d1e394daf8030b6c0efb37f3435769abb49521d
Samlach22 <samlach22@gmail.com>
2021-01-12 22:07:22 +0100

    Utilisation du SDK Discord pour le Rich Presence
    Implémentation d'ouverture et de fermeture du programme discordSDK.exe
    /!\ NECESSITE DANS LE SETUP L'INSTALLATION DE .NET @Mahtwo  @Zok0ta

--- YADUSTOCK/YADUSTOCK/UI_Home.xaml.cs
+++ YADUSTOCK/YADUSTOCK/UI_Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,6 +49,16 @@ EventHandler(SystemEvents_DisplaySettingsChanged);  //Détecte un changement de 
         {
             MainWindow window = (MainWindow)Application.Current.MainWindow;
             window.ButtonClickSound();
+
+            // FIN DE PROCESS DISCORD SDK
+            Process[] workers = Process.GetProcessesByName("DiscordSdk");
+            foreach (Process worker in workers)
+            {
+                worker.Kill();
+                worker.WaitForExit();
+                worker.Dispose();
+            }
+
             Environment.Exit(0);
         }
 
